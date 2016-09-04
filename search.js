b5f2b9ae69cb9a879c48948444cf3e2ba675bf15
Frank Lemanschik <frank@github.dspeed.eu>
2016-09-04 17:42:40 +0100

    Create search.js

---
+++ search.js
@@ -0,0 +1,28 @@
+/*
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "WebSite",
+  "url": "https://www.example.com/",
+  "potentialAction": [{
+    "@type": "SearchAction",
+    "target": "https://query.example.com/search?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  },{
+    "@type": "SearchAction",
+    "target": "android-app://com.example/https/query.example.com/search/?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }]
+}
+</script>
+*/
+
+{
+  "@context": "http://schema.org",
+  "@type": "WebSite",
+  "url": "https://www.example.com/",
+  "potentialAction": [{
+    "@type": "SearchAction",
+    "target": "https://query.example.com/search?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
